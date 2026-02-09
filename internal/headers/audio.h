f421091fc4a4e73fab30dad65f08dbd01efc47c5
lily-cdev <therealarcticsnow@gmail.com>
2026-02-09 06:36:33 -0800

    fish

--- internal/headers/audio.h
+++ internal/headers/audio.h
@@ -1,27 +1,38 @@
 #pragma once
 #include <miniaudio.h>
 #include <stdbool.h>
+#define LDE_AUDIOCOUNT 4;
+#define LDE_FORMAT ma_format_f32
+#define LDE_CHANNELS 2
+#define LDE_SAMPLERATE 44100
 
 typedef struct {
-    ma_sound Data;
 	bool Allocated;
+	bool Looping;
+	bool Playing;
+	float Volume;
+    ma_decoder Data;
+} Subsound;
+
+typedef enum {
+	Ambience1,
+	Filtration1,
+	Ram1,
+	Click
 } Sound;
 
 typedef struct {
-	ma_engine Audio_Engine;
-	ma_engine_config Configuration;
-	Sound Primary_Ambience;
-	Sound Filtration_Loop;
-	Sound Ram_Loop;
-	Sound Click;
+	ma_device Engine;
+	Subsound Data[LDE_AUDIOCOUNT];
+	ma_mutex Lock;
 } AUDIO;
 
 extern AUDIO Audio;
 
-int Startup_Miniaudio();
+ma_result Startup_Miniaudio();
 void Shutdown_Miniaudio();
-void Load_Sound(const char* Path, Sound* Target);
-void Play_Sound(Sound* Target, bool Looping);
-void Terminate_Sound(Sound* Target);
+ma_result Load_Sound(const char* Path, Sound Target);
+void Play_Sound(Sound Target, bool Looping);
+void Terminate_Sound(Sound Target);
 void Adjust_Sound(float Volume);
-void Free_Sound(Sound* Target);
+void Free_Sounds();
--- internal/source/audio.c
+++ internal/source/audio.c
@@ -1,44 +1,88 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include <audio.h>
+#define get_frame_bytes ma_get_bytes_per_frame
+#define read_frames ma_decoder_read_pcm_frames
+#define seek_frames ma_decoder_seek_to_pcm_frame
 
-int Startup_Miniaudio() {
-	Audio.Configuration = ma_engine_config_init();
-	Audio.Configuration.sampleRate = 44100;
-	Audio.Configuration.channels = 2;
-	Audio.Configuration.periodSizeInFrames = 1024;
-	return ma_engine_init(&Audio.Configuration, &Audio.Audio_Engine);
+void Callback(ma_device* Device, void* Output, void* Input, ma_uint32 Frames) {
+	memset(Output, 0, get_frame_bytes(Device->playback.format, Device->playback.channels) * Frames);
+	ma_mutex_lock(&Audio.Lock);
+	for (int C1 = 0; C1 < LDE_AUDIOCOUNT; C1++) {
+		if (Audio.Data[C1].Playing) {
+			ma_uint64 Subframes;
+			read_frames(&(Audio.Data[C1].Data), Output, Frames, &Subframes);
+			for (int C2 = 0; C2 < Subframes * Device->playback.channels; C2++) {
+				Output[C2] *= Audio.Data[C1].Volume;
+			}
+			if (Subframes < Frames) {
+				(Audio.Data[C1].Looping) ? seek_frames(&(Audio.Data[C1].Data), 0) : Audio.Data[C1].Playing = false;
+			}
+		}
+	}
+	ma_mutex_unlock(&Audio.Lock);
+	(void)Input;
+}
+
+ma_result Startup_Miniaudio() {
+	ma_device_config Config = ma_device_config_init(ma_device_type_playback);
+	Config.playback.format = LDE_FORMAT;
+	Config.playback.channels = LDE_CHANNELS;
+	Config.sampleRate = LDE_SAMPLERATE;
+	Config.dataCallback = Callback;
+	ma_result Yield = ma_device_init(NULL, &Config, &Audio.Engine);
+	if (Yield != MA_SUCCESS) {
+		return Yield;
+	}
+	Yield = ma_mutex_init(&Audio.Lock);
+	if (Yield != MA_SUCCESS) {
+		return Yield;
+	}
+	return ma_device_start(&Audio.Engine);
 }
 
 void Shutdown_Miniaudio() {
-	ma_engine_uninit(&Audio.Audio_Engine);
+	ma_device_uninit(&Audio.Audio_Engine);
+	ma_mutex_uninit(&Audio.Lock);
 }
 
-void Load_Sound(const char* Path, Sound* Target) {
+ma_result Load_Sound(const char* Path, Sound Target) {
 	char Carrier[128];
 	snprintf(Carrier, sizeof(Carrier), "Assets/Core/Audio/%s.wav", Path);
-	memset(&(Target->Data), 0, sizeof(ma_sound));
-	if (ma_sound_init_from_file(&Audio.Audio_Engine, Carrier, 0, NULL, NULL, &(Target->Data)) != MA_SUCCESS) {
-		puts("error");
-	}//here
-	Target->Allocated = true;
+	Audio.Data[Target].Volume = 0.5f;
+	Audio.Data[Target].Playing = false;
+	Audio.Data[Target].Looping = false;
+	Audio.Data[Target].Allocated = true;
+	ma_decoder_config Config = ma_decoder_config_init(LDE_FORMAT, LDE_CHANNELS, LDE_SAMPLERATE);
+	return ma_decoder_init_file(Carrier, &Config, &(Audio.Data[Target].Data));
 }
 
-void Play_Sound(Sound* Target, bool Looping) {
-	ma_sound_set_looping(&(Target->Data), (Looping) ? MA_TRUE : MA_FALSE);
-	ma_sound_start(&(Target->Data));
+void Play_Sound(Sound Target, bool Looping) {
+	ma_mutex_lock(&Audio.Lock);
+	Audio.Data[Target].Looping = Looping;
+	Audio.Data[Target].Playing = true;
+	ma_decoder_seek_to_pcm_frame(&(Audio.Data[Target].Data), 0);
+	ma_mutex_unlock(&Audio.Lock);
 }
 
-void Terminate_Sound(Sound* Target) {
-	ma_sound_stop(&(Target->Data));
+void Terminate_Sound(Sound Target) {
+	ma_mutex_lock(&Audio.Lock);
+	Audio.Data[Target].Playing = false;
+	ma_mutex_unlock(&Audio.Lock);
 }
 
 void Adjust_Sound(float Volume) {
-	ma_engine_set_volume(&Audio.Audio_Engine, Volume);
+	ma_mutex_lock(&Audio.Lock);
+	for (int C1 = 0; C1 < LDE_AUDIOCOUNT; C1++) {
+		Audio.Data[C1].Volume = Volume;
+	}
+	ma_mutex_unlock(&Audio.Lock);
 }
 
-void Free_Sound(Sound* Target) {
-	if (Target->Allocated) {
-		ma_sound_uninit(&(Target->Data));
-		Target->Allocated = false;
+void Free_Sounds() {
+	for (int C1 = 0; C1 < LDE_AUDIOCOUNT; C1++) {
+		if (Audio.Data[C1].Allocated) {
+			ma_decoder_uninit(&(Audio.Data[C1].Data));
+			Audio.Data[C1].Allocated = false;
+		}
 	}
 }
